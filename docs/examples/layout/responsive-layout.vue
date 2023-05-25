ec7151c536fa13506da1cc18e7b8b7f4a54158e2
V-cxl <94680346+V-cxl@users.noreply.github.com>
2023-05-25 17:53:56 +0800

    Update responsive-layout.vue

--- docs/examples/layout/responsive-layout.vue
+++ docs/examples/layout/responsive-layout.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row :gutter="10">
-    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+    <el-col :xs="10" :sm="6" :md="4" :lg="3" :xl="1"
       ><div class="grid-content ep-bg-purple"
     /></el-col>
     <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
@@ -9,7 +9,7 @@
     <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
       ><div class="grid-content ep-bg-purple"
     /></el-col>
-    <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+    <el-col :xs="6" :sm="6" :md="4" :lg="3" :xl="1"
       ><div class="grid-content ep-bg-purple-light"
     /></el-col>
   </el-row>
